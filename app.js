0deb59ab9a8c1c58fa4a4e8142630508eb131456
Kev <legui.kevin@gmail.com>
2021-02-15 12:06:09 -0300

    Se agrega cors

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const indexRefugios = require('./routes/refugios');
-const indexMascotas = require('./routes/mascotas');
+const indexRouter = require("./routes/index");
+const indexRefugios = require("./routes/refugios");
+const indexMascotas = require("./routes/mascotas");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(
+  require("cors")({
+    origin: function (origin, callback) {
+      callback(null, origin);
+    },
+    credentials: true,
+  })
+);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/refugios', indexRefugios);
-app.use('/mascotas', indexMascotas);
+app.use("/", indexRouter);
+app.use("/refugios", indexRefugios);
+app.use("/mascotas", indexMascotas);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,4 @@ router.get('/', function(req, res, next) {
   res.send('Hi!');
 });
 
-router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-  next();
-});
-
 module.exports = router;
